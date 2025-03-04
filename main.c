85749633e07e21e51cc8c5b860e464bc0f1ac833
Eric Rivera <riverae4@tcnj.edu>
2025-03-04 21:35:33 +0000

    start main.c file

---
+++ main.c
@@ -0,0 +1,16 @@
+/***
+ * Chris Toala & Eric Rivera
+ * Project 2: Multithreaded Programming
+ * Dr. Yoon
+ * 2/4/25
+ * Description: XXX
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+     
