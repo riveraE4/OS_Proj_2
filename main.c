ba7ffabd53247b3346eae41884917195559b8c25
riveraE4 <riverae4@tcnj.edu>
2025-03-08 17:22:07 -0500

    instead of normal reading and output, outputs into a 2d array to set up for validity checking of row, column, square, and entire sudoko, will implement checking features next

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <time.h>
 
 #define SIZE 9
+#define SQUARE 3
 
 typedef struct {
     int row;
@@ -21,17 +22,21 @@ int main(int argc, char** argv)
 {
     char *filename = "input.txt";
     FILE *file = fopen(filename, "r");
-    int numSud;
-    printf("BOARD STATE IN: %s\n", filename);
-    while ((numSud = fgetc(file)) != EOF) 
-        putchar(numSud);
-        printf("\n");
-        printf("SOLUTION: XXX\n");
+    int sudoko[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++) { // loads in the sudoku into a 2d array
+        for (int j = 0; j < SIZE; j++) {
+            fscanf(file, "%d", &sudoko[i][j]);
+        }
+    }
     fclose(file);
 
-
-
-
-    
+    printf("BOARD STATE IN: %s\n", filename);
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d ", sudoko[i][j]);
+        }
+        printf("\n");
+    }
+    printf("SOLUTION: XXX\n");
     return 0;
 }
